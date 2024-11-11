f3556572da9605f5253f3b4fd6cd266859780f37
AntonObryvalin <catalystqy@gmail.com>
2024-11-11 14:15:28 +0600

    Частичная чистка

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -11,36 +11,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Set;
 
-/**
- * Класс SuccessUserHandler обрабатывает успешную аутентификацию пользователя.
- * <p>
- * В зависимости от роли пользователя перенаправляет его на соответствующую страницу.
- * </p>
- */
 @Component
 public class SuccessUserHandler implements AuthenticationSuccessHandler {
 
-    /**
-     * Метод вызывается после успешной аутентификации.
-     *
-     * @param request        HTTP-запрос
-     * @param response       HTTP-ответ
-     * @param authentication объект {@link Authentication}, содержащий информацию о пользователе
-     * @throws IOException      если происходит ошибка ввода-вывода
-     * @throws ServletException если происходит ошибка сервлета
-     */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        // Получение ролей пользователя
+        
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
-        // Перенаправление пользователя в зависимости от его роли
         if (roles.contains("ROLE_ADMIN")) {
             response.sendRedirect("/admin");
         } else if (roles.contains("ROLE_USER")) {
             response.sendRedirect("/user");
         } else {
-            response.sendRedirect("/"); // Если роль не распознана, перенаправляем на корень
+            response.sendRedirect("/"); 
         }
     }
 }
